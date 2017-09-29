8d12b247f3fafa360c4b7378762ebf5354da2efd
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-09-29 17:12:49 +0300

    Initial

--- jni/jffi/endian.h
+++ jni/jffi/endian.h
@@ -1,8 +1,8 @@
-/* 
+/*
  * Copyright (C) 2009 Wayne Meissner
- * 
+ *
  * This file is part of jffi.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * 
+ *
  * Alternatively, you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -38,6 +38,10 @@
 
 #ifdef __linux__
 #  include_next <endian.h>
+#endif
+
+#if defined(ANDROID) || defined(__ANDROID__)
+#include <endian.h>
 #endif
 
 #ifdef __sun
